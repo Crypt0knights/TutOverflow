750a24cba255ef38915617ca39e948024b48d7b9
nightwarrior-xxx <amanv1999@gmail.com>
2018-11-06 01:49:51 +0530

    Modified the UI

--- blog/write.php
+++ blog/write.php
@@ -4,43 +4,47 @@
 	<title>Blog</title>
 	<link rel="stylesheet" type="text/css" href="blog.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<script type="text/javascript" src="ckeditor/ckeditor.js"></script>
 </head>
 <body>
 	<form method="POST" action="admin.php" enctype="multipart/form-data">
 		
 
 
-
+<!--Title-->
 		<div class="container-fluid form-group">
 			<div>
 				<br>
 		<input type="text" name="title" placeholder="Title" class="box" /><br><br>	
 			</div>
 
+<!--Text Editor-->
 
-
-		<div>
+		<div >
+			<div class="content">Share your Experience here
 			<textarea rows="50" cols="60" placeholder="Share your Experience here" name="content" id="content"></textarea>
-			<br><br>
+			 <script type="text/javascript">
+        		CKEDITOR.replace( 'content' );
+      		</script>
+  		</div>
+			<br>
 		</div>
 
-
-
-
-
-
-
-
-
-
+<!--Author-->
 
 			<div>
 		<input type="text" name="author" placeholder="Author" class="box" /><br><br>
 			</div>
+
+<!--Image-->
+
 			<div>
 				<input type="hidden" name="size" value="1000000">
 		<input type="file" name="image" placeholder="Add a suitable image" class="box" /><br><br>
 			</div>
+
+<!--Publish Button-->
+
 			<div>
 		<button type="submit" class="publish" name="publish">Publish</button>
 			</div>
