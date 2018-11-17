ff74fd0cf2bd12cf235a724607e36cc2643192e2
nightwarrior-xxx <amanv1999@gmail.com>
2018-11-17 18:56:57 +0530

    Assembling of blog with website

--- display.php
+++ display.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <head>
 	<meta charset="utf-8">
-	<meta http-equiv="refresh" content="500">
+	<meta http-equiv="refresh" content="1000">
 	<title>Blog</title>
 	<link rel="stylesheet" type="text/css" href="display.css">
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 </head>
 <body>
+<a href="index.html" class="linkers"><button class="btn btn-default btn-danger" type="button">TutOverFlow</button></a>
+<a href="write.php" class="linkers"><button class="btn btn-default btn-info float-right" type="button">Write Blog</button></a>
 <?php
 $c=mysqli_connect('localhost','root','','blogs');
 if($c){
@@ -14,8 +16,6 @@ if($c){
 	$r=mysqli_query($c,$q); 
 	while($data=mysqli_fetch_array($r)){
 		echo "<div class='container'>";
-			
-
 				echo "<div class='title'>";
 					echo  $data['title']."<br>";
 						echo "<div class ='container-fluid'>";
@@ -23,18 +23,14 @@ if($c){
 							echo "<div class='date'>".$data['date_pub']."</div>";
 						echo "</div>";
 				echo "</div>";
-			
-
 				echo "<div class='pic'>";
 					echo "<img src ='uploads/".$data['image']."'>";	
 				echo "</div>";
-
 		echo "</div>";
 # for printing the content
 				echo "<div class='container1 section_content'>";
 					echo htmlspecialchars_decode(stripcslashes($data['content']));
 				echo "</div>";
-         		
 		echo "<br><br>"; 
 	}
 }
